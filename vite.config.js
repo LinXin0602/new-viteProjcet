ccb19d7d0cd5f408b3a87dde656107622e648cd2
LinXin <nk004851634@gmail.com>
2023-05-29 13:45:58 +0800

    github Actions

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'LinXin0602',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
