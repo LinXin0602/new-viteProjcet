e21e5fc3ca83ffc0453b56ce58c7cd5fdce78327
LinXin <nk004851634@gmail.com>
2023-05-29 13:30:06 +0800

    router createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/loginmanage',
       name: 'loginmanage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('@/views/Backstage/LoginManage.vue'),
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('@/views/Backstage/DashBoard.vue'),
       children: [
         {
