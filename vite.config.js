52ee8e9b75cfce8c91c711dd62e91decfd69fcc8
LinXin <nk004851634@gmail.com>
2023-05-29 14:14:27 +0800

    更改vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'LinXin0602',
+  base: 'new-viteProjcet',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
